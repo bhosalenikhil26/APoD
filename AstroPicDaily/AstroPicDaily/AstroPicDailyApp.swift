f8bc614ee6c155ba997317c7ab4594505ccab251
Nikhil Bhosale <nikhil.bhosale@voiapp.io>
2024-05-11 13:14:38 +0200

    Created XCode Project

---
+++ AstroPicDaily/AstroPicDaily/AstroPicDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPicDailyApp.swift
+//  AstroPicDaily
+//
+//  Created by Nikhil Bhosale on 2024-05-11.
+//
+
+import SwiftUI
+
+@main
+struct AstroPicDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroPicDaily/AstroPicDaily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AstroPicDaily
+//
+//  Created by Nikhil Bhosale on 2024-05-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
