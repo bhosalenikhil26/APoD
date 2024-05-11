04faf3fa69baae36531fa0b21ae6b50d7d79dd2b
Nikhil Bhosale <nikhil.bhosale@voiapp.io>
2024-05-11 20:51:27 +0200

    Added Cache implementation to cache images

--- AstroPicDaily/AstroPicDaily/Utilities/ImageCache.swift
+++ AstroPicDaily/AstroPicDaily/Utilities/ImageCache.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ImageCacheProtocol {
+    func set(_ image: UIImage, forKey key: String)
+    func get(forKey key: String) -> UIImage?
+}
+
+final class ImageCache {
+    static let shared = ImageCache()
+    private let cache = NSCache<NSString, UIImage>()
+
+    private init() {}
+}
+
+extension ImageCache: ImageCacheProtocol {
+    func set(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+
+    func get(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+}
+
