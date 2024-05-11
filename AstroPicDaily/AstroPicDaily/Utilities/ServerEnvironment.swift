7d56ffaad3e93552e1da4bec939209042e9321b4
Nikhil Bhosale <nikhil.bhosale@voiapp.io>
2024-05-11 15:43:40 +0200

    -Update minimum target version to 15 -Added utility code so that it becomes reusable.

---
+++ AstroPicDaily/AstroPicDaily/Utilities/ServerEnvironment.swift
@@ -0,0 +1,12 @@
+//
+//  ServerEnvironment.swift
+//  Nations
+//
+//  Created by Nikhil Bhosale on 2024-05-11.
+//
+
+final class ServerEnvironment {
+    static var serverUrl: String {
+        "https://api.nasa.gov/"
+    }
+}
